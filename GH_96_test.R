1e7b11fed47aaf7d1e2639c2d58c80bb389e3200
Matt Brigida <matt@complete-markets.com>
2015-05-23 12:19:00 -0400

    initial commit

---
+++ GH_96_test.R
@@ -0,0 +1,35 @@
+## The Gregory and Hansen (1996) test for a single shift in the parameters of a cointegrating relationship.
+
+library(EIAdata)
+library(tseries) 
+
+## Get EIA API key ----
+key <- source('~/eia_key')$value
+
+## getting data to test ----
+ng <- getEIA('NG.RNGWHHD.W', key=key)
+oil <- getEIA('PET.RCLC1.W', key = key)
+
+## merge so we have data over a common time interval ----
+data <- merge.xts(ng, oil, join = 'inner')
+names(data) <- c("ng", "oil")
+
+oil <- log(data$oil)
+gas <- log(data$ng)
+
+## create a lower triangular matrix which has 1 on all the nonzero entries.  There is certainly a non-'for loop' way to do this. ----
+ind <- matrix(0, nrow=length(oil), ncol=length(oil))
+for(i in 1:length(oil)){
+    ind[,i] <- c(rep(0,i),rep(1,(length(oil)-i)))}
+
+## run an Augmented Dickey-Fuller test on the residuals of every regression ----
+## each regression has a structural break at a different time over all possible times ----
+adf.m2 <- 0
+for(i in 1:length(oil)){
+    adf.m2[i] <- adf.test(lm(as.vector(gas)~ind[,i]+as.vector(oil))$resid)$statistic}
+
+stat <- min(adf.m2)
+
+## Now need to compare stat to the critical value ----
+
+
